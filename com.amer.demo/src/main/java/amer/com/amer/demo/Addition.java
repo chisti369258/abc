b6cbdfea3839b62d62dd07bcca66d5e323dd67b0
syed amer chisti <syedamer693@gmail.com>
2019-07-14 11:57:00 -0700

    addition if two number

---
+++ com.amer.demo/src/main/java/amer/com/amer/demo/Addition.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package amer.com.amer.demo;
+
+/**
+ * @author syeda
+ *
+ */
+
+public class Addition {
+	
+	public int add() {
+		int a=10;
+		int b=20;
+		int c = a+b;
+		return c;
+	}
+	
+
+}
